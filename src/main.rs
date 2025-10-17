f5148a3894822c9adcb1e966fb9dd4bc2bff3154
Daniel Rush <dmrush529@gmail.com>
2025-10-17 16:12:24 -0600

    print the files test

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 //utility to empty temp folder on windows
 //intended to be user-generic, so anyone could use
 use whoami::username;
+use std::fs::{read_dir, remove_file};
 
 //main function to get full file path to start with
 fn main() {
     let username = username();
     let path = format!("C:\\Users\\{}\\AppData\\Local\\Temp", username);
 
-    println!("full user path to temp folder: {}", path);
+    let files = read_dir(path).unwrap();
+    for file in files {
+        println!("{}", file.unwrap().path().display());
+    }
 }
