16ef82869d39427af53af27a2daaf1d49c6b003b
Daniel Rush <dmrush529@gmail.com>
2025-10-17 16:08:31 -0600

    first attempt

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@
 //intended to be user-generic, so anyone could use
 use whoami::username;
 
+//main function to get full file path to start with
 fn main() {
     let username = username();
-    println!("Hello, {}!", username);
+    let path = format!("C:\\Users\\{}\\AppData\\Local\\Temp", username);
+
+    println!("full user path to temp folder: {}", path);
 }
